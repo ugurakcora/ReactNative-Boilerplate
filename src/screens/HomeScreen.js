4cdff4fe63cd901d9b98df2d95b3ff31d888e6dd
ugurakcora <uakcora97@gmail.com>
2023-10-11 02:33:05 +0300

    init expo ⭐

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return <Text style={styles.text}>First Project</Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    color: "red",
+    fontWeight: "bold",
   },
 });
 
